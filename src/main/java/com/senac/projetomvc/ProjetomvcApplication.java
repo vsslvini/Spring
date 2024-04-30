5305caab0629170596b9f2f35868ba36d1a774b5
Vinicius <vsslviniciussousa@gmail.com>
2024-04-29 23:16:32 -0300

    Configuração inicia do projeto

---
+++ src/main/java/com/senac/projetomvc/ProjetomvcApplication.java
@@ -0,0 +1,13 @@
+package com.senac.projetomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/projetomvc/ProjetomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.projetomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
